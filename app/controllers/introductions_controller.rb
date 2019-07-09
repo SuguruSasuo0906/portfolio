982efd9484bf96cb5b13086bc768537cfb73be5e
SuguruSasuo <fingertail0906@gmail.com>
2019-07-10 00:27:47 +0900

    edit

--- app/controllers/introductions_controller.rb
+++ app/controllers/introductions_controller.rb
@@ -10,7 +10,7 @@ class IntroductionsController < ApplicationController
       flash[:success] = 'お問い合わせを受け付けました'
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
