69c648fb133f1c4a7c7c1fb2884eb1dd552be861
SuguruSasuo <fingertail0906@gmail.com>
2019-07-14 15:38:11 +0900

    edit other

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,4 @@ class Contact < ApplicationRecord
   validates :email, presence: true,  format: { with: VALID_EMAIL_REGEX,message: "英文字のみが使用できます" }
   validates :name, length: { maximum: 20 }, presence: true
   validates :message, presence: true
-  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
-  validates :tell, presence: true, format: { with: VALID_PHONE_REGEX }
 end
